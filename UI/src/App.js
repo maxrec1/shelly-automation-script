f7aeace4b4c1565bc986943a4e2c67d753fe062f
Max <146830724+maxrec1@users.noreply.github.com>
2024-06-19 15:03:18 +0200

    Add files via upload

---
+++ UI/src/App.js
@@ -0,0 +1,43 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { MyProSidebarProvider } from "./pages/global/sidebar/sidebarContext";
+import Topbar from "./pages/global/Topbar";
+import Dashboard from "./pages/dashboard";
+import Client from "./pages/client";
+import Form from "./pages/form";
+import Bar from "./pages/bar";
+import Line from "./pages/line"; 
+import Pie from "./pages/pie";
+import Geography from "./pages/geography";
+import Devices from "./pages/devices";
+
+const App = () => {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MyProSidebarProvider>
+          <div style={{ height: "100%", width: "100%" }}>
+            <main>
+              <Topbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/client" element={<Client />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/devices" element={<Devices/>}/>
+                <Route path="/geography" element={<Geography />} />
+              </Routes>
+            </main>
+          </div>
+        </MyProSidebarProvider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export default App;
