804d40a84001111d205fd8c5534b4f14c72291da
Max <146830724+maxrec1@users.noreply.github.com>
2024-06-28 13:32:13 +0200

    Update mqtt_to_database1.py

--- Dashboard/scripts/mqtt_to_database1.py
+++ Dashboard/scripts/mqtt_to_database1.py
@@ -3,9 +3,9 @@ import json
 import mysql.connector
 
 # MQTT Broker details
-broker_address = "test-vzwgj6.a01.euc1.aws.hivemq.cloud"
-broker_port = 8883
-username = "microdain1"
+broker_address = "185.164.4.216"
+broker_port = 1883
+username = "microdain"
 password = "Fenix2017"
 
 # MySQL database details
@@ -14,14 +14,35 @@ db_user = "microdain"
 db_password = "Fenix2017"
 db_name = "shelly_database1"
 
-def create_table(cursor, topic):
-    table_name = topic.replace('-', '_')  # Replace hyphens with underscores
-    table_name = table_name.replace('shellyplusplugs_', '')  # Remove "mqtt_" prefix
+def create_table(cursor, table_name):
     print("Creating table:", table_name)
-    sql = f"CREATE TABLE IF NOT EXISTS {table_name} (id INT AUTO_INCREMENT PRIMARY KEY, city VARCHAR(255), day VARCHAR(255), date DATE, time TIME, isHoliday VARCHAR(255), isActive VARCHAR(255), power FLOAT, CO2 FLOAT)"
+    sql = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        country VARCHAR(255),
+        city VARCHAR(255),
+        day VARCHAR(255),
+        date DATE,
+        time TIME,
+        isHoliday VARCHAR(255),
+        isActive VARCHAR(255),
+        power FLOAT,
+        CO2 FLOAT,
+        client VARCHAR(255) DEFAULT NULL,
+        location VARCHAR(255) DEFAULT NULL
+    )
+    """
     print("SQL statement:", sql)
     # Execute SQL statement
     cursor.execute(sql)
+
+def ensure_columns(cursor, table_name):
+    columns = ['country', 'client', 'location']
+    for column in columns:
+        cursor.execute(f"SHOW COLUMNS FROM {table_name} LIKE '{column}'")
+        result = cursor.fetchone()
+        if not result:
+            cursor.execute(f"ALTER TABLE {table_name} ADD {column} VARCHAR(255) DEFAULT NULL")
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -40,7 +61,9 @@ def on_message(client, userdata, msg):
     cursor = conn.cursor()
 
     # Create table if it does not exist
-    create_table(cursor, topic)
+    table_name = topic.replace('-', '_').replace('shellyplusplugs_', '')
+    create_table(cursor, table_name)
+    ensure_columns(cursor, table_name)
 
     # Convert power from string to float
     power_value = float(data["power"].replace('Wh', ''))
@@ -48,9 +71,11 @@ def on_message(client, userdata, msg):
     time_value = data["time"][:5]  # Truncate the time string to "HH:MM"
 
     # Insert data into the corresponding table
-    table_name = topic.replace('-', '_').replace('shellyplusplugs_', '')  # Remove "mqtt_" prefix
-    sql = f"INSERT INTO {table_name} (city, day, date, time, isHoliday, isActive, power, CO2) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-    val = (data["city"], data["day"], data["date"], time_value, data["isHoliday"], data["isActive"], power_value, co2_value)
+    sql = f"""
+    INSERT INTO {table_name} (country, city, day, date, time, isHoliday, isActive, power, CO2)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    val = (data["country"], data["city"], data["day"], data["date"], time_value, data["isHoliday"], data["isActive"], power_value, co2_value)
     cursor.execute(sql, val)
     conn.commit()
 
@@ -61,7 +86,6 @@ client.username_pw_set(username, password)
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.tls_set()
 client.connect(broker_address, broker_port, 60)
 
 client.loop_forever()
